0a3cd3746fe6c674083d63e51d613220ae894cc3
Wanderrer <rwboshae@gmail.com>
2018-07-28 02:42:32 -0700

    Changed to functional component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-class About extends React.Component {
-  render(){
+const About = () => {
     return(
       <div className="about" >
-        <h1>Hi there, I'm Rick Boshae</h1>
-        <p>I am a software engineer living in Los Angeles, California. I love it here. I also love making software, collaborating with others, and learning new stuff. I have a variety of hobbies but some of my favorites things to do are developing applications for Android and the Web. Check out my site to learn more about me.</p>
+        <h1>About Me</h1>
+        <p>Hi there, I'm Rick Boshae. I am a software engineer living in Los Angeles, California. I love it here. I also love making software, collaborating with others, and learning new stuff. I have a variety of hobbies but some of my favorites things to do are developing applications for Android and the Web. Check out my site to learn more about me.</p>
         </div>
     );
-  }
 }
 
 export default About;
