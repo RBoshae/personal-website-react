a180308c9ab3efdd4358aee6c857d662acbb8fea
Wanderrer <rwboshae@gmail.com>
2018-08-01 19:05:32 -0700

    Added Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Experience = ()=> {
   return (
     <div>
-      <h1>Experience</h1>
+      <h1>Technical Skills</h1>
+      <h3>Software</h3>
+      <p>Android Studio, Git, GitHub, MATLAB, XCode</p>
+      <h3>Programming</h3>
+      <p>C, C++, C#, CSS, HTML, Java, JavaScript, JSON, jQuery, PostgreSQL, Python, React, SQL</p>
     </div>
   );
 }
