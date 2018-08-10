d3af3ee60f5f14da2736ab6d2968c69dc0f18dd5
Wanderrer <rwboshae@gmail.com>
2018-08-10 16:18:34 -0700

    Updates

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/rick/MyPrograms/WebDev/rboshae.github.io/src/layouts/index.js"
-  import data from "/home/rick/MyPrograms/WebDev/rboshae.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
