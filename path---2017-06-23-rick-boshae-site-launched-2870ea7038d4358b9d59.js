cddfa56247f40ce9c067aebb228f455c797115e8
Wanderrer <rwboshae@gmail.com>
2018-09-27 01:09:32 -0700

    Updates

--- path---2017-06-23-rick-boshae-site-launched-2870ea7038d4358b9d59.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([22720662086050],{352:function(t,a){t.exports={data:{markdownRemark:{html:'<p>Well. Finally got around to putting this old website together. Neat thing about it - powered by <a href="http://jekylrb.com">Jekyll</a> and I can use Markdown to author my posts. It actually is a lot easier than I thought it was going to be.</p>',frontmatter:{title:"Rick Boshae, Launches Site"}}},pathContext:{slug:"/2017-06-23-rick-boshae-site-launched/"}}}});
-//# sourceMappingURL=path---2017-06-23-rick-boshae-site-launched-2870ea7038d4358b9d59.js.map
--- path---2017-06-23-rick-boshae-site-launched-fe8596a6a775d7933c2b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([22720662086050],{358:function(t,a){t.exports={data:{markdownRemark:{html:'<p>Well. Finally got around to putting this old website together. Neat thing about it - powered by <a href="http://jekylrb.com">Jekyll</a> and I can use Markdown to author my posts. It actually is a lot easier than I thought it was going to be. </p>',frontmatter:{title:"Rick Boshae, Launches Site"}}},pathContext:{slug:"/2017-06-23-rick-boshae-site-launched/"}}}});
-//# sourceMappingURL=path---2017-06-23-rick-boshae-site-launched-fe8596a6a775d7933c2b.js.map
