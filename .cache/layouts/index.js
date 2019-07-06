2c6c5942a8ebf9c443927747021336c4c6cb301e
rboshae <rwboshae@gmail.com>
2019-07-06 14:03:43 -0700

    .gitignore fix

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/wanderer/Development/WebDev/rboshae.github.io/src/layouts/index.js"
-  import data from "/home/wanderer/Development/WebDev/rboshae.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
