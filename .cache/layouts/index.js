649fa8f4f4a79f972397e388d41b81b61bdfdc91
rboshae <rwboshae@gmail.com>
2019-07-06 13:42:23 -0700

    Updating site

--- .cache/layouts/index.js
+++ .cache/layouts/index.js
@@ -1,7 +1,7 @@
 
   import React from "react"
-  import Component from "/home/rick/MyPrograms/WebDev/rboshae.github.io/src/layouts/index.js"
-  import data from "/home/rick/MyPrograms/WebDev/rboshae.github.io/.cache/json/layout-index.json"
+  import Component from "/home/wanderer/Development/WebDev/rboshae.github.io/src/layouts/index.js"
+  import data from "/home/wanderer/Development/WebDev/rboshae.github.io/.cache/json/layout-index.json"
 
   export default (props) => <Component {...props} {...data} />
   
